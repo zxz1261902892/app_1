3d8744685e9b68e5f0f7b5ea493ef72f677bd266
zxz1261902892 <50995053+zxz1261902892@users.noreply.github.com>
2019-05-27 11:42:24 +0800

    Update test_002.py

--- scripts/test_002.py
+++ scripts/test_002.py
@@ -6,6 +6,6 @@ class Test_002:
     def test_add_png(self):
         """添加一张abc.png截图到测试报告"""
 
-        with open("/Users/mac/Documents/Worker/sh-app8-day08/scripts/abc.png", "rb") as f:
+        with open("/Users/mac/Desktop/sh-app8-day08/scripts/abc.png", "rb") as f:
             # 添加图片内容到测试报告，以附件的方式
             allure.attach("截图", f.read(), allure.attach_type.PNG)
